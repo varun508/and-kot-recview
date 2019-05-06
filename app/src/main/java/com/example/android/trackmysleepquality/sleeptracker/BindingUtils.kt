2e6787733f825abae77d0d6a65c8b6473311260b
Varun <imva508@gmail.com>
2019-05-07 01:43:06 +0530

    added data binding to the list item layout

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
@@ -15,3 +15,37 @@
  */
 
 package com.example.android.trackmysleepquality.sleeptracker
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.convertDurationToFormatted
+import com.example.android.trackmysleepquality.convertNumericQualityToString
+import com.example.android.trackmysleepquality.database.SleepNight
+
+@BindingAdapter("sleepDurationFormatted")
+fun TextView.setSleepDurationFormatted(night: SleepNight?) {
+    night?.let { text = convertDurationToFormatted(it.startTimeMilli, it.endTimeMilli, context.resources) }
+}
+
+@BindingAdapter("sleepQualityString")
+fun TextView.setSleepQualityString(night: SleepNight?) {
+    night?.let { text = convertNumericQualityToString(it.sleepQuality, context.resources) }
+}
+
+
+@BindingAdapter("sleepImage")
+fun ImageView.setSleepImage(night: SleepNight?) {
+    night?.let {
+        setImageResource(when (it.sleepQuality) {
+            0 -> R.drawable.ic_sleep_0
+            1 -> R.drawable.ic_sleep_1
+            2 -> R.drawable.ic_sleep_2
+            3 -> R.drawable.ic_sleep_3
+            4 -> R.drawable.ic_sleep_4
+            5 -> R.drawable.ic_sleep_5
+            else -> R.drawable.ic_sleep_active
+        })
+    }
+}
